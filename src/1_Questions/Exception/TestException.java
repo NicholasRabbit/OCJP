1b64037dece7e3352b7ab00600232dd84f1d52c4
plms <wuyangzerg@gmail.com>
2024-11-11 15:45:04 +0800

    add dependencies of JUnit

---
+++ src/1_Questions/Exception/TestException.java
@@ -0,0 +1,36 @@
+package com.test.exception;
+
+public class TestException {
+
+    public static void main(String[] args) {
+
+        int arr[] = new int[4];
+
+        arr[0] = 1;
+
+        arr[1] = 2;
+
+        arr[2] = 4;
+
+        arr[3] = 5;
+
+        int sum = 0;
+
+        try {
+
+            for (int pos = 0; pos <= 4; pos++) {
+
+                sum = sum + arr[pos];
+
+            }
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+
+		//上面异常被捕捉后，方法不会结束，这里还会打印输出。
+        System.out.println(sum);
+
+    }
+
+}
