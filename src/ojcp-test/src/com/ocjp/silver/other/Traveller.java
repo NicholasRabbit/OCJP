2711862d0adea22f8a4fc0c45033f736734a3e71
plms <wuyangzerg@gmail.com>
2025-02-08 14:17:55 +0800

    keep on learning

--- src/ojcp-test/src/com/ocjp/silver/other/Traveller.java
+++ src/ojcp-test/src/com/ocjp/silver/other/Traveller.java
@@ -3,7 +3,7 @@ package com.ocjp.silver.other;
 //Q132
 class Tour {
     public static void main(String[] args) {
-        System.out.println("Happy journey!" + args[1]);
+        System.out.println("Happy journey! " + args[1]);
     }
 }
 
