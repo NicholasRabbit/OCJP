9777547e257b8ea490ae25197089e7dde66dce30
plms <wuyangzerg@gmail.com>
2025-03-10 16:57:46 +0800

    learning

---
+++ src/ojcp-test/src/com/ocjp/silver/questions/q183/Test_183.java
@@ -0,0 +1,16 @@
+package com.ocjp.silver.questions.q183;
+
+public class Test_183 {
+
+    public static void main(String[] args) {
+        float f1 = 100F;
+        float f2 = (float) 1_11.00;
+        float f3 = 100;
+        double y1 = 100.0;
+//        float f4 = y1;   // compilation error
+        int y2 = 100;
+        float f5 = (float) y2;
+
+
+    }
+}
